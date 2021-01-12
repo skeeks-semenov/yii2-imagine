81cd4180b3b3452e76789b45693bf6e0d85f92fb
Semenov <semenov@skeeks.com>
2021-01-12 11:41:07 +0300

    dev

---
+++ src/BaseImage.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\imagine;
+
+use Yii;
+use Imagine\Image\Box;
+use Imagine\Image\Color;
+use Imagine\Image\ImageInterface;
+use Imagine\Image\AbstractImagine;
+use Imagine\Image\ManipulatorInterface;
+use Imagine\Image\Point;
+use yii\base\InvalidConfigException;
+use yii\base\InvalidParamException;
+use yii\helpers\ArrayHelper;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class BaseImage extends \yii\imagine\BaseImage
+{}
---
+++ src/Image.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link https://cms.skeeks.com/
+ * @copyright Copyright (c) 2010 SkeekS
+ * @license https://cms.skeeks.com/license/
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+
+namespace skeeks\imagine;
+
+/**
+ * @author Semenov Alexander <semenov@skeeks.com>
+ */
+class Image extends BaseImage
+{
+}
